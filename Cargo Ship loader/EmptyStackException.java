e3b025e8e5402cca474850cdbd9cf6a809687f78
imalan123 <54748329+imalan123@users.noreply.github.com>
2022-02-07 14:51:18 -0500

    Update EmptyStackException.java

--- Cargo Ship loader/EmptyStackException.java
+++ Cargo Ship loader/EmptyStackException.java
@@ -1,11 +1,7 @@
 /** IllegalArgumentException
  *
- * Homework 3 CSE214
  *
  * @author Alan Jin
- *      email: alan.jin@stonybrook.edu
- *      Stony Brook ID: 112578370
- *      recitation # : R04
  */
 
 /**Extension of Illegal Argument Exception
