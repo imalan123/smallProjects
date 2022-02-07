6ca4176b9d5897e74c9e775822f89c5cfb75a466
imalan123 <54748329+imalan123@users.noreply.github.com>
2022-02-07 14:52:09 -0500

    Update ShipLoader.java

--- Cargo Ship loader/ShipLoader.java
+++ Cargo Ship loader/ShipLoader.java
@@ -1,11 +1,7 @@
 /** The ShipLoader object implements CargoShip, CargoStack, Cargo, and CargoStrength objects for the user to use
  *
- * Homework 3 CSE214
  *
  * @author Alan Jin
- *      email: alan.jin@stonybrook.edu
- *      Stony Brook ID: 112578370
- *      recitation # : R04
  */
 
 import java.util.Scanner;
